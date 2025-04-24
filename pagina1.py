4ba6f876ddd898acae4fbdde196553ff8307e31c
Lucaonascimento15 <134176313+Lucaonascimento15@users.noreply.github.com>
2025-04-24 07:07:55 -0300

    Add files via upload

---
+++ pagina1.py
@@ -0,0 +1,14 @@
+titulo = ("My Rotine App")
+
+
+tarefa1 = print(input = ("Adicione uma tarefa"))
+tarefa2 = 0
+tarefa3 = 0 
+tarefa4 = 0
+tarefa5 = 0
+
+lista_tarafas = [tarefa1,tarefa2,tarefa3,tarefa4,tarefa5]
+qtd_tarefas = len
+sucesso = ("Você adicionou {} tarefas ao seu dia".format(qtd_tarefas))
+
+print(sucesso)
